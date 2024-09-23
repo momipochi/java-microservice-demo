ad5ff83922c0d4726607db4cadc0c0c961c7af6a
unknown <k25650727@gmail.com>
2024-09-23 16:03:44 +0200

    commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,39 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@RestController
+@EnableScheduling
 public class DemoApplication {
 
-	public static void main(String[] args) throws InterruptedException {
-		Thread.sleep(10000);
+	@Profile("usage_message")
+	@Bean
+	public CommandLineRunner usage() {
+		return args -> {
+			System.out.println("This app uses Spring Profiles to control its behavior.\n");
+			System.out.println(
+					"Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
+		};
+	}
+
+	@Profile("!usage_message")
+	@Bean
+	public CommandLineRunner tutorial() {
+		return new RabbitAmqpTutorialsRunner();
+	}
+
+	public static void main(String[] args) throws InterruptedException, IOException, TimeoutException {
+		// Thread.sleep(10000);
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
---
+++ rabbitmq_demo/src/main/java/com/example/rabbitmq_demo/rabbitmq_demo/RabbitmqDemoApplication.java
@@ -0,0 +1,36 @@
+package com.example.rabbitmq_demo.rabbitmq_demo;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+@SpringBootApplication
+public class RabbitmqDemoApplication {
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] args) throws IOException, TimeoutException {
+		// SpringApplication.run(RabbitmqDemoApplication.class, args);
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+			String message = new String(delivery.getBody(), "UTF-8");
+			System.out.println(" [x] Received '" + message + "'");
+		};
+		channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
+		});
+	}
+
+}
